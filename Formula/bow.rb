bf87d918d5f71e3fb9d687c62d6c5ec0b37ef69a
Rasmus Winther Zakarias <rasmus.zakarias@chainalysis.com>
2026-01-21 11:58:09 +0100

    add real Bow formula

--- Formula/bow.rb
+++ Formula/bow.rb
@@ -1,13 +1,18 @@
 class Bow < Formula
-  desc "Plow through the muddy waters for shipping"
-  homepage "https://github.com/rasmus-zakarias/bow"
-  url "https://github.com/rasmus-zakarias/bow/archive/refs/tags/0.0.1.tar.gz"
+  on_macos do
+    if Hardware::CPU.arm?
+      version "0.1.8-26"
+      desc "Chainalysis Bow - fast and confident shipping"
+      homepage "https://github.com/chainalysis/bow"
+      url "https://fimbulvetr.chainalysis.com/artifactory/generic-test-local/bow-0.1.8-26-go1.25.1-darwin-arm64.tar.xz",
+      headers: [
+              "Authorization: Bearer #{ENV["HOMEBREW_ARTIFACTORY_TOKEN"]}"
+      ]
+      sha256 "e57b50aa0c6b49cae133252fc76137dcdb46e7949dc31af1009d61378359a48f"
 
-  def install
-    bin.install "bow"
-  end
-  
-  test do
-    assert_match "Chainlysis Bow", shell_output("#{bin}/bow", 2)
+      def install
+          bin.install "bin/bow"
+      end
+    end
   end
 end
