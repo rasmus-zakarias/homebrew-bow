4f7496991dd5b39971612a150df959a6c06ee7f4
Rasmus Winther Zakarias <rasmus.zakarias@chainalysis.com>
2026-01-23 11:48:55 +0100

    release homebrew

--- Formula/bow.rb
+++ Formula/bow.rb
@@ -1,14 +1,14 @@
 class Bow < Formula
   on_macos do
     if Hardware::CPU.arm?
-      version "0.1.9-0"
+      version "0.1.9-1"
       desc "Chainalysis Bow - fast and confident shipping"
       homepage "https://github.com/chainalysis/bow"
-      url "https://fimbulvetr.chainalysis.com/artifactory/generic-test-local/bow-0.1.9-0-go1.25.1-darwin-arm64.tar.xz",
+      url "https://fimbulvetr.chainalysis.com/artifactory/generic-test-local/bow-0.1.9-1-go1.25.1-darwin-arm64.tar.xz",
       headers: [
               "Authorization: Bearer #{ENV["HOMEBREW_ARTIFACTORY_TOKEN"]}"
       ]
-      sha256 "f3297b00904e599215ca7f8eae334f79dd819c90482b671fe95143bf3bb63533"
+      sha256 "05db911ae3ae25bacc79b07d54b96a01c2fe79a5b8f2ae06692bebbae4231ee0"
 
       def install
           bin.install "bin/bow"
